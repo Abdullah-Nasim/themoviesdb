1af3c58e5d65c82477e3f404a75fb319a3d3d6aa
Abdullah <abdullah.nasim@netaq.ae>
2017-10-05 20:14:35 +0400

    Completed Project Structure

---
+++ app/src/main/java/myown/themoviesdb/AppController.java
@@ -0,0 +1,23 @@
+package myown.themoviesdb;
+
+import android.app.Application;
+
+/**
+ * Created by Netaq on 10/5/2017.
+ */
+
+public class AppController extends Application {
+
+    /**
+     * The AppController is automatically called every time when the application is loaded.
+     * We will initialize all of the Application level dependencies in onCreate method of this class.
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Initializing Fresco
+
+    }
+
+}
--- app/src/main/java/myown/themoviesdb/activities/MainActivity.java
+++ app/src/main/java/myown/themoviesdb/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package myown.themoviesdb;
+package myown.themoviesdb.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import myown.themoviesdb.R;
 
 public class MainActivity extends AppCompatActivity {
 
